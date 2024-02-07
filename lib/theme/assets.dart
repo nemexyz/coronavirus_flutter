bc155b8b406ceb1f1a6784e43729899291b0b230
Axel Cantor <nemexyz@gmail.com>
2024-02-07 10:12:50 -0500

    Tema oscuro listo

--- lib/theme/assets.dart
+++ lib/theme/assets.dart
@@ -1,5 +1,6 @@
 class Images {
   static const String logom = 'assets/images/logom.webp';
+  static const String logomAlpha = 'assets/images/logom_alpha.png';
   static const String femenino = 'assets/images/femenino.webp';
   static const String masculino = 'assets/images/masculino.jpg';
   static const String google = 'assets/images/google.png';
--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -3,4 +3,10 @@ import 'package:flutter/material.dart';
 class ThemeColors {
   static const primaryColor = Color(0xFFEE8429);
   static const grey = Color(0xFF404040);
+  static const grey2 = Color(0xFF707070);
+  static const lightGrey = Color(0xFFBFBFBF);
+  static const lightestGrey = Color(0xFFBFBFBF);
+  static const darkGrey = Color(0xFF333333);
+  static const white = Color(0xFFFFFFFF);
+  static const black = Color(0xFF000000);
 }
